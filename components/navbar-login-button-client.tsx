317bde829a4ab6d16d5d014249c4d18aa54a91b3
Edier Gama <ediersantiago7@gmail.com>
2024-08-29 00:13:16 -0500

    feat: :sparkles: add profile picture of google account

--- components/navbar-login-button-client.tsx
+++ components/navbar-login-button-client.tsx
@@ -4,17 +4,18 @@
 "use client"
 import { Link } from "@nextui-org/link";
 import { NavbarItem } from "@nextui-org/navbar";
+import {Avatar} from "@nextui-org/avatar"
 
 import { SignInWithGoogle } from "../utils/supabase/sign-in-with.google";
 
 export default function NavbarLoginButtonClient({ user }: any) {
   const session = user
-
+  
   return (
       <section>
         {(session !== null) && (
           <section>
-            <h2>perfil not null</h2>
+            <Avatar isBordered src={session.user.user_metadata.picture} />
           </section>
         )}
         {(session === null) && (
